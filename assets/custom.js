d40bddba241d613d6646ed742201946381bc3ada
AhsanQureshi48 <53149590+AhsanQureshi48@users.noreply.github.com>
2022-12-20 14:30:31 +0500

    demo

---
+++ assets/custom.js
@@ -0,0 +1,109 @@
+$(document).ready(function () {
+  $(".featured_slider").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 500,
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".people_block_slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    speed: 500,
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".clean_block_slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 500,
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".sinuplus_mn_slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 500,
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".blog__posts.articles-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 500,
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
